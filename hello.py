835168c0baf97eee2e69a55e8cefd9fabe393ce1
sabari12nath <sabarinathps6@gmail.com>
2025-06-12 14:55:17 +0530

    changed to simple math operation

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+# Simple program to demonstrate basic Python concepts
+
+# Print a greeting
+name = print(input("Enter your name: "))
+print("Hello, {name}!")
+
+# Basic math operations
+number = int(input("Enter a number: "))
+print("Double of {number} is: {number * 2}")
+print("Square of {number} is: {number ** 2}")
+
+# Simple if-else statement
+if number > 10:
+    print("That's a big number!")
+else:
+    print("That's a small number!")
